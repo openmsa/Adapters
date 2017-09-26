e4b1a263797623f42fb87a284c7ff4983fd590b4
UBiqube Ireland PLC <ubiqube_git@ubiqube.com>
2017-09-26 16:33:36 +0100

    Add files via upload

---
+++ linux_generic/php/provisioning_stages.php
@@ -0,0 +1,11 @@
+<?php
+
+$provisioning_stages = array(
+0 => array('name' => 'Lock Provisioning',     'prog' => 'prov_lock'),
+1 => array('name' => 'Initial Connection',    'prog' => 'prov_init_conn'),
+2 => array('name' => 'Initial Configuration', 'prog' => 'prov_init_conf'),
+3 => array('name' => 'Unlock Provisioning',   'prog' => 'prov_unlock'),
+4 => array('name' => 'Save Configuration',    'prog' => 'prov_save_conf'),
+);
+
+?>
