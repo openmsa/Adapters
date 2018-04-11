9ae78484a38adfefa831221006a861bc8747315f
Sergio Lebon <sergio.lebon@hpe.com>
2018-04-11 15:32:49 +0200

    Remane and update AWS device adapter.

---
+++ aws/common.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Date : Oct 19, 2007
+ */
+
+// Script description
+require_once 'smsd/sms_common.php';
+
+
+?>
--- aws/do_cmd_snmpwalk.php
+++ aws/do_cmd_snmpwalk.php
@@ -9,10 +9,10 @@
   *  $optional_params	  optional parameters
   *  $sms_module    module name (for patterns)
   */
- 
- // Verb JSACMD XXX
+
+ // Verb JSACMD SNMPWALK
  require_once 'smsd/sms_common.php';
  require_once load_once('smsd', 'do_cmd_snmpwalk.php');
  return SMS_OK;
- 
+
  ?>
