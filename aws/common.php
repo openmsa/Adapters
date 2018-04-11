0db367446a185876c3735aa06da84f8302048845
Sergio Lebon <sle@ubiqube.com>
2018-04-11 15:32:49 +0200

    aws: copy from wsa => aws

---
+++ aws/common.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+ * Date : Jun 26, 2012
+*/
+
+function get_wsa_error($err_string)
+{
+  $error_wsa_string = htmlentities(substr($err_string, strpos($err_string, 'ERROR:'), (strlen($err_string) - strpos($err_string, '*****'))*(-1)));
+  return $error_wsa_string;
+}
+?>
