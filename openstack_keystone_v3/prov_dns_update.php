e7190d26c8210a5a52df42bb49427cbfb62405ca
ofa <ofa@ubiqube.com>
2018-07-25 15:05:20 +0200

    update openstack from MSA-16.2.2-P12
    - Fix for openstack_keystone_v3_connect => domain id -> name
    - Fixed typos and cleanup in Openstack DA
    - Cleanup Openstack device adaptors, return openstack API call response from device in executeCommand response

--- openstack_keystone_v3/prov_dns_update.php
+++ openstack_keystone_v3/prov_dns_update.php
@@ -5,6 +5,8 @@
 // -------------------------------------------------------------------------------------
 function prov_dns_update($sms_csp, $sdid, $sms_sd_info, &$err)
 {
+  global $ipaddr;
+
   return sd_ip_update($sms_csp, $sdid, $sms_sd_info, $ipaddr);
 }
 
