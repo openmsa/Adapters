b31c7f934798f6b036b591d0c66c822305af6950
ofa <ofa@ubiqube.com>
2018-09-30 19:23:32 +0200

    fix concat syntax error (and spacing) in error message

--- paloalto_generic/paloalto_generic_connect.php
+++ paloalto_generic/paloalto_generic_connect.php
@@ -222,7 +222,7 @@ class DeviceConnection extends GenericConnection
         {
           if ($palo_retry_show_limit <= 0)
           {
-            sms_log_error(__FILE__ . ':' . __LINE__ . ' : Giving up after . '$palo_retry_configured_limit . 'times (no status FIN received)');
+            sms_log_error(__FILE__ . ':' . __LINE__ . ' : Giving up after ' . $palo_retry_configured_limit . ' times (no status FIN received)');
             break;
           }
           $palo_retry_show_limit--;
