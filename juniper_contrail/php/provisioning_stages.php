5950025a498e6cd5d2a2bb27d0f77fc0eaaf061e
antoine-brun <antoine.brun@gmail.com>
2018-10-09 13:57:06 +0200

    added DA for Juniper Contrail SDN controller

---
+++ juniper_contrail/php/provisioning_stages.php
@@ -0,0 +1,9 @@
+<?php
+
+$provisioning_stages = array(
+  0 => array('name' => 'Lock Provisioning',     'prog' => 'prov_lock'),
+  1 => array('name' => 'Conf init',   'prog' => 'prov_init_conf'),
+  2 => array('name' => 'Unlock Provisioning',   'prog' => 'prov_unlock'),
+);
+
+?>
