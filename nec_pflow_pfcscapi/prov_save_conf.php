e8f03c2c947855b35d2d4dbcd98121b83e7614a4
ofa <ofa@ubiqube.com>
2019-01-18 00:14:47 +0100

    nec_pflow_pfcscapi: upgrade prov_*.php to new format

--- nec_pflow_pfcscapi/prov_save_conf.php
+++ nec_pflow_pfcscapi/prov_save_conf.php
@@ -3,16 +3,9 @@
 // -------------------------------------------------------------------------------------
 // SAVE CONFIGURATION
 // -------------------------------------------------------------------------------------
-function prov_save_conf($sms_csp, $sdid, $sms_sd_info, $stage)
+function prov_save_conf($sms_csp, $sdid, $sms_sd_info, &$err)
 {
-  $ret = exec_local(__FILE__.':'.__LINE__, "/opt/sms/bin/save_router_conf \"update\" $sdid CONF_FILE", $output);
-  if ($ret !== SMS_OK)
-  {
-    sms_bd_set_provstatus($sms_csp, $sms_sd_info, $stage, 'F', $ret, null, "");
-    return $ret;
-  }
-
-  return SMS_OK;
+  return sd_commit_conf(__FILE__.':'.__LINE__, $sdid, 'Initial Provisioning Backup', 'CONF_FILE', $err);
 }
 
 ?>
--- nec_pflow_pfcscapi/prov_unlock.php
+++ nec_pflow_pfcscapi/prov_unlock.php
@@ -3,16 +3,9 @@
 // -------------------------------------------------------------------------------------
 // UNLOCK PROVISIONING
 // -------------------------------------------------------------------------------------
-function prov_unlock($sms_csp, $sdid, $sms_sd_info, $stage)
+function prov_unlock($sms_csp, $sdid, $sms_sd_info, &$err)
 {
-  $ret = sms_sd_prov_unlock($sms_csp, $sms_sd_info);
-  if ($ret != SMS_OK)
-  {
-    sms_bd_set_provstatus($sms_csp, $sms_sd_info, $stage, 'F', $ret, null, "");
-    return $ret;
-  }
-  sms_sd_forceasset($sms_csp, $sms_sd_info);
-  return SMS_OK;
+  return sd_prov_unlock($sms_csp, $sms_sd_info);
 }
 
 ?>
