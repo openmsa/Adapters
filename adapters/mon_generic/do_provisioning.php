75a3900fd29a0a15fcf7491b3131d1a2ab863736
antoine-brun <antoine.brun@gmail.com>
2020-08-27 15:26:31 +0200

    add new line, remove header

--- adapters/mon_generic/do_provisioning.php
+++ adapters/mon_generic/do_provisioning.php
@@ -1,14 +1,4 @@
 <?php
-/*
- * Version: $Id$
- * Created: May 30, 2008
- * Available global variables
- *  $sms_sd_info        sd_info structure
- * 	$sms_sd_ctx         pointer to sd_ctx context to retreive usefull field(s)
- *  $sms_csp            pointer to csp context to retreive usefull field(s)
- *  $sdid
- *  $sms_module         module name (for patterns)
- */
 
 // Initial provisioning
 
@@ -185,3 +175,4 @@ sms_sd_forceasset($sms_csp, $sms_sd_info);
 
 return 0;
 ?>
+
--- adapters/mon_generic/provisioning_stages.php
+++ adapters/mon_generic/provisioning_stages.php
@@ -1,15 +1,4 @@
 <?php
-/*
- * Version: $Id$
- * Created: Jun 27, 2008
- * Available global variables
- *  $sms_sd_ctx        pointer to sd_ctx context to retreive usefull field(s)
- *  $sms_sd_info        sd_info structure
- *  $sms_csp            pointer to csp context to send response to user
- *  $sdid
- *  $sms_module         module name (for patterns)
- *  $SMS_RETURN_BUF    string buffer containing the result
- */
 
 // Enter Script description here
 require_once 'polld/common.php';
