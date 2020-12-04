f764fd8e9bc2b968cef199fb943800851ecb989a
root <devops@openmsa.co>
2020-12-04 09:15:03 +0000

    Redfish Generic was removed

--- adapters/redfish_generic/provisioning_stages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$provisioning_stages = array(
-  array('name' => 'Lock Provisioning',     'prog' => 'prov_lock'),
-  array('name' => 'Initial Connection',    'prog' => 'prov_init_conn'),
-  array('name' => 'Unlock Provisioning',   'prog' => 'prov_unlock')
-);
-
-?>
