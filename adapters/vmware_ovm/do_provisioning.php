f0332641778d0695bcde40f77bcd938782fa0a63
antoine-brun <antoine.brun@gmail.com>
2020-12-22 16:40:51 +0100

    add implementation

--- adapters/vmware_ovm/do_provisioning.php
+++ adapters/vmware_ovm/do_provisioning.php
@@ -22,9 +22,29 @@ require_once load_once('linux_generic', 'adaptor.php');
 require_once load_once('linux_generic', 'common.php');
 require_once load_once('linux_generic', 'linux_generic_configuration.php');
 require_once load_once('vmware_ovm', 'provisioning_stages.php');
-
 require_once "$db_objects";
 
-return require_once 'linux_generic/do_provisioning.php';
+$network = get_network_profile();
+$SD = &$network->SD;
+
+if($SD->SD_MANAGEMENT_PORT !== 0)
+{
+  $port = $SD->SD_MANAGEMENT_PORT;
+}
+else
+{
+  $port = 22;
+}
+
+// -------------------------------------------------------------------------------------
+// USER PARAMETERS CHECK
+// -------------------------------------------------------------------------------------
+if (empty($ipaddr) || empty($login) || empty($passwd)  || empty($port))
+{
+  sms_send_user_error($sms_csp, $sdid, "addr=$ipaddr login=$login pass=$passwd adminpass=$adminpasswd port=$port", ERR_VERB_BAD_PARAM);
+  return SMS_OK;
+}
+
+return require_once 'smsd/do_provisioning.php';
 
 ?>
