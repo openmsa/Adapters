a2e22d295e76d6df3440f9aa00b9f085ea416734
Sergio Lebon <sle@ubiqube.com>
2021-02-03 16:15:47 +0100

    MSA-9596: init Terraform CLI generic adaptor code.

---
+++ adapters/terraform_generic/polld/terraform_generic_mgmt.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Date : Sep 24, 2007
+ * Available global variables
+ *  $sms_sd_info       sd_info structure
+ *  $sdid
+ *  $sms_module        module name (for patterns)
+ *  $sd_poll_elt       pointer on sd_poll_t structure
+ *  $sd_poll_peer      pointer on sd_poll_t structure of the peer (slave of master)
+ */
+
+// Script description
+require_once 'smsd/sms_common.php';
+require_once load_once('terraform_generic', 'adaptor.php');
+require_once "$db_objects";
+
+function exit_error($line, $error)
+{
+  sms_log_error("$line: $error\n");
+  sd_disconnect();
+  exit($error);
+}
+
+
+try
+{
+  // Connection
+  sd_connect();
+
+  $asset['firmware'] = sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "showversion");
+
+  $line = get_one_line($buffer);
+  while ($line !== false)
+  {
+    // regular asset fields
+    foreach ($show_ver_asset_patterns as $name => $pattern)
+    {
+    		if (preg_match($pattern, $line, $matches) > 0)
+    		{
+    		  $asset[$name] = trim($matches[$name]);
+    		}
+    }
+
+    // remove already used patterns
+    if (isset($asset))
+    {
+    		foreach ($asset as $name => $value)
+    		{
+    		  unset($show_ver_asset_patterns[$name]);
+    		}
+    }
+
+    $line = get_one_line($buffer);
+  }
+
+  $ret = sms_polld_set_asset_in_sd($sd_poll_elt, $asset);
+  if ($ret !== 0)
+  {
+    exit_error(__FILE__ . ':' . __LINE__, ": sms_polld_set_asset_in_sd($sms_sd_ctx, $asset) Failed\n");
+  }
+
+
+  sd_disconnect();
+}
+catch (Exception | Error $e)
+{
+  sd_disconnect();
+  exit($e->getCode());
+}
+
+return 0;
+
+?>
---
+++ adapters/terraform_generic/terraform_generic_connect.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * 	Version: 0.1: terraform_generic_connect.php
+ *  	Created: Jun 7, 2012
+ *  	Available global variables
+ *  	$sms_sd_ctx        	pointer to sd_ctx context to retreive usefull field(s)
+ *  	$sms_sd_info        	sd_info structure
+ *  	$sms_csp            	pointer to csp context to send response to user
+ *  	$sdid
+ *  	$sms_module         	module name (for patterns)
+ *  	$SMS_RETURN_BUF    	string buffer containing the result
+ */
+
+// Open/close the session dialog with the router
+// For communication with the router
+require_once 'smsd/sms_common.php';
+require_once 'smsd/expect.php';
+require_once 'smsd/ssh_connection.php';
+require_once 'smsd/telnet_connection.php';
+require_once load_once('terraform_generic', 'common.php');
+require_once "$db_objects";
+
+// return false if error, true if ok
+function terraform_generic_connect($sd_ip_addr = null, $login = null, $passwd = null, $adminpasswd = null, $port_to_use = null)
+{
+  global $sms_sd_ctx;
+
+  try
+  {
+    $sms_sd_ctx = new TerraformSSHConnection($sd_ip_addr, $login, $passwd, $adminpasswd, $port_to_use);
+    $sms_sd_ctx->setParam("PROTOCOL", "SSH");
+  }
+  catch (SmsException $e)
+  {
+    debug_dump($e);
+    return $e->getCode();
+  }
+  return SMS_OK;
+}
+
+// Disconnect
+// return false if error, true if ok
+function terraform_generic_disconnect()
+{
+  $sms_sd_ctx = null;
+  return SMS_OK;
+}
+
+function terraform_generic_synchro_prompt()
+{
+  global $sms_sd_ctx;
+
+  $msg = 'UBISyncro' . mt_rand(10000, 99999);
+  $prompt = $sms_sd_ctx->getPrompt();
+  sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "showversion");
+}
+
+class TerraformSSHConnection extends SshConnection
+{
+  public function do_store_prompt()
+  {
+    $this->setParam("newline_dos", true);
+
+    global $sendexpect_result;
+    echo "TerraformSSHConnection.do_store_prompt\n";
+    $tab[0] = '>';
+    $index = sendexpect(__FILE__ . ':' . __LINE__, $this, 'showversion', $tab);
+    echo " 1 sendexpect\n";
+
+    $this->prompt = trim($sendexpect_result);
+    if (strrchr($this->prompt, "\n") !== false)
+    {
+       $this->prompt = substr(strrchr($this->prompt, "\n"), 1);
+    }
+
+    echo "Prompt found: {$this->prompt} for {$this->sd_ip_config}\n";
+
+    // synchronize again
+    $prompt = $this->prompt;
+    sendexpectone(__FILE__ . ':' . __LINE__, $this, "showversion");
+
+  }
+
+  public function do_start() {
+      $this->setParam('chars_to_remove', array("\033[00m", "\033[m"));
+  }
+
+}
+
+
+
+?>
