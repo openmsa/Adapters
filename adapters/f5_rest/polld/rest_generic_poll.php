f622ee3ed1d6a90c05789908cb4e8836aea53419
root <devops@openmsa.co>
2021-03-23 13:59:10 +0000

    F5 BIG-IP REST API DA have been added

---
+++ adapters/f5_rest/polld/rest_generic_poll.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'smsd/sms_common.php';
+require_once load_once('rest_generic', 'rest_generic_connect.php');
+
+require_once "$db_objects";
+
+try
+{
+
+  rest_generic_connect();
+  rest_generic_disconnect();
+  return SMS_OK;
+}
+catch (Exception | Error $e)
+{
+  $msg = $e->getMessage();
+  $code = $e->getCode();
+  sms_log_error("connection error : $msg ($code)");
+  return $code;
+}
+?>
