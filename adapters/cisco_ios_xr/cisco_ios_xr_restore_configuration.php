d9e55396702bd98fd0b3d12950e056c2f7677b31
yguubiqube <ygu@ubiqube.com>
2021-12-02 14:21:31 +0000

    MSA-10710: restore conf using the "load" command and not reboot with modified startup conf.

--- adapters/cisco_ios_xr/cisco_ios_xr_restore_configuration.php
+++ adapters/cisco_ios_xr/cisco_ios_xr_restore_configuration.php
@@ -8,6 +8,9 @@ require_once load_once ( 'cisco_ios_xr', 'cisco_ios_xr_connect.php' );
 require_once load_once ( 'cisco_ios_xr', 'apply_errors.php' );
 
 require_once "$db_objects";
+
+define('DELAY', 200000);
+
 class cisco_ios_xr_restore_configuration {
 	var $conf_path; // Path for previous stored configuration files
 	var $sdid; // ID of the SD to update
@@ -51,10 +54,8 @@ class cisco_ios_xr_restore_configuration {
 		}
 
 		$res = array_to_string ( $output );
-		// echo("TITI:$res\n");
-		// echo "TOTO\n";
 
-		// replace hidden credentials in SVN... otherwise we losse connection
+		// replace hidden credentials in SVN... otherwise we lose connection
 		$patterns = array ();
 
 		// SHOULD MATCH:
@@ -94,9 +95,13 @@ class cisco_ios_xr_restore_configuration {
 
 	function restore_conf() {
 		global $apply_errors;
+		global $default_disk;
 
 		global $sms_sd_ctx;
 		$ret = SMS_OK;
+
+		// destination for configuration file on device
+		$dst_disk = $default_disk;
 
 		echo "SCP mode configuration\n";
 
@@ -115,31 +120,113 @@ class cisco_ios_xr_restore_configuration {
 		try {
 			$ret = scp_to_router ( $full_name, $file_name );
 			if ($ret === SMS_OK) {
-			    // SCP OK
-				$SMS_OUTPUT_BUF = copy_to_running ( "copy disk0:$file_name startup-config" );
-				save_result_file ( $SMS_OUTPUT_BUF, "conf.error" );
+				// SCP OK
 
-				foreach ( $apply_errors as $apply_error ) {
-					if (preg_match ( $apply_error, $SMS_OUTPUT_BUF ) > 0) {
-						sms_log_error ( __FILE__ . ':' . __LINE__ . ": [[!!! $SMS_OUTPUT_BUF !!!]]\n" );
-						return ERR_SD_CMDFAILED;
-					}
-				}
+				echo "Load configuration to restore\n";
+				$ERROR_BUFFER = '';
 
-				unset ( $tab );
-				$tab [0] = $sms_sd_ctx->getPrompt ();
-				$tab [1] = "]?";
-				$tab [2] = "[confirm]";
-				$index = sendexpect ( __FILE__ . ':' . __LINE__, $sms_sd_ctx, "delete disk0:$file_name", $tab );
-				while ( $index !== 0 ) {
-					$index = sendexpect ( __FILE__ . ':' . __LINE__, $sms_sd_ctx, "", $tab );
-				}
+				sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "conf exclusive", "(config)#", DELAY);
+				/*
+					RP/0/RP0/CPU0:DEV-NEC-CISCO-IOS-XR-9000(config)#load disk0:UBI153.cfg
+					Loading.
+					1263 bytes parsed in 1 sec (1246)bytes/sec
+				 or
+					RP/0/RP0/CPU0:DEV-NEC-CISCO-IOS-XR-9000(config)#load disk0:toto.txt
+					Loading.
+					Syntax/Authorization errors in one or more commands. Please use 'show configuration failed load [detail]' to view errors.
 
-				func_reboot ( 'restore configuration' );
+					6 bytes parsed in 1 sec (5)bytes/sec
+				*/
+				$line = "load $dst_disk:$file_name";
+				$SMS_OUTPUT_BUF = sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, $line, "(config)#", DELAY);
+
+				if (preg_match("@Syntax/Authorization errors@", $SMS_OUTPUT_BUF, $matches) > 0) {
+                    $ERROR_BUFFER .= "!";
+                    $ERROR_BUFFER .= "\n";
+                    $ERROR_BUFFER .= $line;
+                    $ERROR_BUFFER .= "\n";
+                    $ERROR_BUFFER .= $SMS_OUTPUT_BUF;
+                    $ERROR_BUFFER .= "\n";
+
+                    $line = "show configuration failed load detail";
+                    $SMS_OUTPUT_BUF = sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, $line, ")#", DELAY);
+
+                    $ERROR_BUFFER .= $line;
+                    $ERROR_BUFFER .= "\n";
+                    $ERROR_BUFFER .= $SMS_OUTPUT_BUF;
+                    $ERROR_BUFFER .= "\n";
+
+                    $ret = ERR_RESTORE_FAILED;
+                }
+                else {
+                    // Commit configuration at revision r$this->revision_id
+
+                    /*
+                    RP/0/RP0/CPU0:DEV-NEC-CISCO-IOS-XR-9000(config)#commit replace comment "load from disk0:UBI153.cfg"
+                    This commit will replace or remove the entire running configuration. This
+                    operation can be service affecting.
+                    Do you wish to proceed? [no]: yes
+
+                    RP/0/RP0/CPU0:DEV-NEC-CISCO-IOS-XR-9000(config)#end
+                    */
+                    $line = "commit replace comment \"MSA: restore conf r$this->revision_id\"";
+                    $SMS_OUTPUT_BUF .= sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, $line, "proceed? [no]:", DELAY);
+
+                    $SMS_OUTPUT_BUF .= sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "yes", ")#", DELAY);
+
+                    foreach ($apply_errors as $apply_error) {
+                        if (preg_match($apply_error, $SMS_OUTPUT_BUF, $matches) > 0) {
+                            $ERROR_BUFFER .= "!";
+                            $ERROR_BUFFER .= "\n";
+                            $ERROR_BUFFER .= $line;
+                            $ERROR_BUFFER .= "\n";
+                            $ERROR_BUFFER .= $apply_error;
+                            $ERROR_BUFFER .= "\n";
+                            $SMS_OUTPUT_BUF = '';
+
+                            $ret = ERR_RESTORE_FAILED;
+                        }
+                    }
+                } // end of: else (preg_match("@Syntax/Authorization errors@", ...)
+
+                // if load or commit failed, we can have message to commit then at end command
+                unset($tab);
+                $tab[0] = $sms_sd_ctx->getPrompt();
+                $tab[1] = "commit them before exiting(yes/no/cancel)? [cancel]:";
+
+                $index = sendexpect(__FILE__ . ':' . __LINE__, $sms_sd_ctx, 'end', $tab, DELAY);
+                if ($index === 1) {
+                    sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "no", "#", DELAY);
+                }
+
+                // Refetch the prompt cause it can change during the apply conf
+                extract_prompt();
+
+                // Exit from config mode
+                unset($tab);
+                $tab[0] = $sms_sd_ctx->getPrompt();
+                $tab[1] = ")#";
+                $index = sendexpect(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "", $tab, DELAY);
+                $SMS_OUTPUT_BUF = $sendexpect_result;
+                for ($i = 1; ($i <= 10) && ($index === 1); $i++) {
+                    $index = sendexpect(__FILE__ . ':' . __LINE__, $sms_sd_ctx, "exit", $tab, DELAY);
+                    $SMS_OUTPUT_BUF .= $sendexpect_result;
+                }
+
+                if (!empty($ERROR_BUFFER)) {
+                    save_result_file($ERROR_BUFFER, "conf.error");
+                    $SMS_OUTPUT_BUF = $ERROR_BUFFER;
+                    sms_log_error(__FILE__ . ':' . __LINE__ . ": [[!!! $SMS_OUTPUT_BUF !!!]]\n");
+                    return ERR_SD_CMDFAILED;
+                }
+                else {
+                    save_result_file("No error found during the application of the configuration", "conf.error");
+                }
 
 				return $ret;
-			} else {
-				// SCP ERROR
+			}
+			else {
+				// SCP error: scp_to_router ( ... ) !== SMS_OK
 				sms_log_error ( __FILE__ . ':' . __LINE__ . ":SCP Error $ret\n" );
 				return $ret;
 			}
@@ -148,42 +235,11 @@ class cisco_ios_xr_restore_configuration {
 				return ERR_SD_CONNREFUSED;
 			}
 			sms_log_error ( __FILE__ . ':' . __LINE__ . ":SCP Error $ret\n" );
-			// if no tftp attempt, we return
-			return ERR_SD_TFTP;
-		}
-
-		/*
-		echo "tftp mode configuration\n";
-
-		$ret = SMS_OK;
-		$sms_ip_addr = $_SERVER ['SMS_ADDRESS_IP'];
-
-		sendexpectone ( __FILE__ . ':' . __LINE__, $sms_sd_ctx, "copy tftp://$sms_ip_addr/$file_name startup-config", "]?" );
-
-		$SMS_OUTPUT_BUF = copy_to_running ( '' );
-		save_result_file ( $SMS_OUTPUT_BUF, "conf.error" );
-
-		foreach ( $apply_errors as $apply_error ) {
-			if (preg_match ( $apply_error, $SMS_OUTPUT_BUF ) > 0) {
-				sms_log_error ( __FILE__ . ':' . __LINE__ . ": [[!!! $SMS_OUTPUT_BUF !!!]]\n" );
-				return ERR_SD_CMDFAILED;
-			}
-		}
-		*/
-
-		if (! strpos ( $SMS_OUTPUT_BUF, 'bytes copied' )) {
-			sms_log_error ( __FILE__ . ':' . __LINE__ . ":tftp transfer failed\n" );
-			return ERR_SD_TFTP;
-		}
-
-		func_reboot ( 'restore configuration' );
-
+			return ERR_SD_SCP;
+		} // end try scp_to_router ( $full_name, $file_name );
 		return $ret;
 	}
-
-	function wait_until_device_is_up() {
-		return wait_for_device_up ( $this->sd->SD_IP_CONFIG );
-	}
+	// end of function restore_conf()
 }
 
 ?>
