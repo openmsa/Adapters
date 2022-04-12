a62fbd5af9cf3f35791ed921e165c29da2796d6b
Sergio Lebon <sle@ubiqube.com>
2022-04-12 10:12:54 +0200

    Update openstack_keystone_v3 adapter.

--- adapters/openstack_keystone_v3/prov_lock.php
+++ adapters/openstack_keystone_v3/prov_lock.php
@@ -5,6 +5,7 @@
 // -------------------------------------------------------------------------------------
 function prov_lock($sms_csp, $sdid, $sms_sd_info, &$err)
 {
+echo 'naveen';
   return sd_prov_lock($sms_csp, $sms_sd_info);
 }
 
