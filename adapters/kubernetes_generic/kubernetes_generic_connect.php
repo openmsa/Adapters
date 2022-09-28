1fb4071d52fd4b44462e386f42a7941fc7baad2e
ydu <ydu@ubiqube.com>
2022-09-28 12:37:00 +0200

    MSA-11765 : [CCLA] AZURE ENV status icon stay grey after creation + Adapt polld & K8 DA

--- adapters/kubernetes_generic/kubernetes_generic_connect.php
+++ adapters/kubernetes_generic/kubernetes_generic_connect.php
@@ -11,60 +11,54 @@ require_once "$db_objects";
 class KubernetesGenericRESTConnection extends GenericConnection
 {
    private $key;
-   private $endPointsURL;
    private $xml_response;
    private $raw_xml;
    private $raw_json;
-   
+
    // ------------------------------------------------------------------------------------------------
    public function do_connect()
    {
        unset($this->key);
-       unset($this->endPointsURL);
-       
-       // first time keystone is forced by removing the endpoint
-       unset($this->endPoint);
-       
+
        $network = get_network_profile();
-       $sd =& $network->SD;
+       $sd = &$network->SD;
+
        $kube_auth_method  = $sd->SD_CONFIGVAR_list['KUBE_AUTH_METHOD']->VAR_VALUE;
-       $tenant_id         = $sd->SD_CONFIGVAR_list['TENANT_ID']->VAR_VALUE;
-       $user_domain_id    = $sd->SD_CONFIGVAR_list['USER_DOMAIN_ID']->VAR_VALUE;
-       $project_domain_id = $sd->SD_CONFIGVAR_list['PROJECT_DOMAIN_ID']->VAR_VALUE;
-       $cmd               = "POST##/v3/auth/tokens#{\"auth\": {\"identity\": {\"methods\": [\"password\"], \"password\": {\"user\": {\"domain\": {\"name\":";
-       $cmd .= "\"{$user_domain_id}\"},\"name\": \"{$this->sd_login_entry}\",\"password\": \"{$this->sd_passwd_entry}\"}}}, ";
-       $cmd .= "\"scope\": {\"project\": {\"domain\": {\"name\": \"{$project_domain_id}\"}, \"id\": \"{$tenant_id}\"}}}}";
+
        if ($kube_auth_method == "KUBERNETES" || $kube_auth_method == "EKS") {
            $cmd = "GET##/api#{}";
+       } else {
+         $tenant_id         = $sd->SD_CONFIGVAR_list['TENANT_ID']->VAR_VALUE;
+         $user_domain_id    = $sd->SD_CONFIGVAR_list['USER_DOMAIN_ID']->VAR_VALUE;
+         $project_domain_id = $sd->SD_CONFIGVAR_list['PROJECT_DOMAIN_ID']->VAR_VALUE;
+         $cmd               = "POST##/v3/auth/tokens#{\"auth\": {\"identity\": {\"methods\": [\"password\"], \"password\": {\"user\": {\"domain\": {\"name\":";
+         $cmd .= "\"{$user_domain_id}\"},\"name\": \"{$this->sd_login_entry}\",\"password\": \"{$this->sd_passwd_entry}\"}}}, ";
+         $cmd .= "\"scope\": {\"project\": {\"domain\": {\"name\": \"{$project_domain_id}\"}, \"id\": \"{$tenant_id}\"}}}}";
        }
-       
-       $result = $this->sendexpectone(__FILE__ . ':' . __LINE__, $cmd, "");
-       
-       $endPointsURL_table = $result->xpath('//token/catalog');
-       
-       $this->endPointsURL = $endPointsURL_table[0];
+
+       $this->sendexpectone(__FILE__ . ':' . __LINE__, $cmd, "");
    }
-   
+
    // ------------------------------------------------------------------------------------------------
    public function sendexpectone($origin, $cmd, $prompt = 'lire dans sdctx', $delay = EXPECT_DELAY, $display_error = true)
    {
        global $sendexpect_result;
        $this->send($origin, $cmd);
-       
+
        if ($prompt !== 'lire dans sdctx' && !empty($prompt)) {
            $tab[0] = $prompt;
        } else {
            $tab = array();
        }
-       
+
        $this->expect($origin, $tab);
-       
+
        if (is_array($sendexpect_result)) {
            return $sendexpect_result[0];
        }
        return $sendexpect_result;
    }
-   
+
    // ------------------------------------------------------------------------------------------------
    public function send($origin, $cmd)
    {
@@ -72,20 +66,20 @@ class KubernetesGenericRESTConnection extends GenericConnection
        unset($this->raw_xml);
        $network = get_network_profile();
        $sd =& $network->SD;
-       
+
        $http_protocol = $sd->SD_CONFIGVAR_list['HTTP_PROTOCOL']->VAR_VALUE;
        if (empty($http_protocol)) {
            $http_protocol = "http";
        }
-       
+
        $kube_http_protocol = $sd->SD_CONFIGVAR_list['KUBE_HTTP_PROTOCOL']->VAR_VALUE;
        $kube_port          = $sd->SD_CONFIGVAR_list['KUBE_PORT']->VAR_VALUE;
        if (empty($kube_http_protocol)) {
            $kube_http_protocol = "http";
        }
-       
+
        $delay = EXPECT_DELAY / 1000;
-       
+
        $action           = explode("#", $cmd);
        $kube_auth_method = $sd->SD_CONFIGVAR_list['KUBE_AUTH_METHOD']->VAR_VALUE;
        $kube_token       = $sd->SD_CONFIGVAR_list['KUBE_TOKEN']->VAR_VALUE;
@@ -95,16 +89,16 @@ class KubernetesGenericRESTConnection extends GenericConnection
        } else {
            $action[2] = $kube_http_protocol . '://' . $this->sd_ip_config . ':' . $kube_port . '' . $action[2];
        }
-       
+
        $token = "";
        if (isset($this->key)) {
            $token = "-H \"X-Auth-Token: {$this->key}\"";
        }
-       
+
        if (!empty($kube_token)) {
            $token = $sd->SD_CONFIGVAR_list['KUBE_TOKEN']->VAR_VALUE;
        }
-       
+
        if ($kube_auth_method == "EKS") {
            $region       = $sd->SD_CONFIGVAR_list['region']->VAR_VALUE;
            $cluster_id   = $sd->SD_CONFIGVAR_list['cluster_id']->VAR_VALUE;
@@ -120,29 +114,29 @@ class KubernetesGenericRESTConnection extends GenericConnection
            $json = json_decode($output_array[0], true);
            $token = $json['status']['token'];
        }
-       
+
        // TODO TEST validité champ ACTION[]
        $curl_cmd = "curl --tlsv1.2 -i -sw '\nHTTP_CODE=%{http_code}' --connect-timeout {$delay} --max-time {$delay} -X {$action[0]} {$token} -H \"Content-Type: application/json\" -k '{$action[2]}'";
        if ($kube_auth_method == "KUBERNETES"|| $kube_auth_method == "EKS") {
            $curl_cmd = "curl --tlsv1.2 -i -sw '\nHTTP_CODE=%{http_code}' --connect-timeout {$delay} --max-time {$delay} -X {$action[0]} --header \"Authorization: Bearer {$token}\" -H \"Content-Type: application/json\" -k '{$action[2]}'";
        }
-       
-       
+
+
        if (isset($action[3]) && ($kube_auth_method == "KUBERNETES"|| $kube_auth_method == "EKS")) {
            $curl_cmd .= " -d '{$action[3]}'";
        }
-       
+
        echo "{$cmd} for endPoint {$action[1]}\n";
-       
+
        $curl_cmd .= " && echo";
        $ret = exec_local($origin, $curl_cmd, $output_array);
-       
+
        if ($ret !== SMS_OK) {
            throw new SmsException("Call to API Failed", $ret);
        }
-       
+
        $result = '';
-       
+
        foreach ($output_array as $line) {
            if ($line !== 'SMS_OK') {
                if (strpos($line, 'HTTP_CODE') !== 0) {
@@ -178,13 +172,13 @@ class KubernetesGenericRESTConnection extends GenericConnection
        } else {
            $response_body = "";
        }
-       
+
        if ($this->content_type == 'application/json') {
            $array = json_decode($response_body, true);
-           
+
            // call array to xml conversion function
            $xml = arrayToXml($array, '<root></root>');
-           
+
            $this->raw_json = $response_body;
        } elseif ($this->content_type == 'text/plain') {
            $result_in_array  = explode("\n", $response_body);
@@ -200,25 +194,25 @@ class KubernetesGenericRESTConnection extends GenericConnection
        } else {
            $xml = new SimpleXMLElement($response_body);
        }
-       
+
        $this->xml_response = $xml; // new SimpleXMLElement($result);
-       
+
        // FIN AJOUT
        $this->raw_xml = $this->xml_response->asXML();
        debug_dump($this->raw_xml, "DEVICE RESPONSE\n");
    }
-   
+
    // ------------------------------------------------------------------------------------------------
    public function sendCmd($origin, $cmd)
    {
        $this->send($origin, $cmd);
    }
-   
+
    // ------------------------------------------------------------------------------------------------
    public function expect($origin, $tab, $delay = EXPECT_DELAY, $display_error = true, $global_result_name = 'sendexpect_result')
    {
        global $$global_result_name;
-       
+
        if (!isset($this->xml_response)) {
            throw new SmsException("$origin: cmd timeout, $tab[0] not found", ERR_SD_CMDTMOUT);
        }
@@ -229,7 +223,7 @@ class KubernetesGenericRESTConnection extends GenericConnection
            return $index;
        }
        foreach ($tab as $path) {
-           
+
            $result = $this->xml_response->xpath($path);
            if (($result !== false) && !empty($result)) {
                $$global_result_name = $result;
@@ -237,10 +231,10 @@ class KubernetesGenericRESTConnection extends GenericConnection
            }
            $index++;
        }
-       
+
        throw new SmsException("$origin: cmd timeout, $tab[0] not found", ERR_SD_CMDTMOUT);
    }
-   
+
    public function get_raw_xml()
    {
        return $this->raw_xml;
@@ -249,7 +243,7 @@ class KubernetesGenericRESTConnection extends GenericConnection
    {
        return $this->raw_json;
    }
-   
+
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -257,7 +251,7 @@ class KubernetesGenericRESTConnection extends GenericConnection
 function kubernetes_generic_connect($sd_ip_addr = null, $login = null, $passwd = null, $port_to_use = null)
 {
    global $sms_sd_ctx;
-   
+
    $sms_sd_ctx = new KubernetesGenericRESTConnection($sd_ip_addr, $login, $passwd, $port_to_use);
    return SMS_OK;
 }
--- adapters/kubernetes_generic/polld/kubernetes_generic_poll.php
+++ adapters/kubernetes_generic/polld/kubernetes_generic_poll.php
@@ -1,6 +1,24 @@
 <?php
 
 require_once 'smsd/sms_common.php';
-return SMS_OK;
+/*
+require_once load_once('kubernetes_generic', 'kubernetes_generic_connect.php');
 
+require_once "$db_objects";
+
+try
+{
+
+  kubernetes_generic_connect();
+  kubernetes_generic_disconnect();
+}
+catch (Exception | Error $e)
+{
+  $msg = $e->getMessage();
+  $code = $e->getCode();
+  sms_log_error("connection error : $msg ($code)");
+  return $code;
+}
+*/
+return SMS_OK;
 ?>
