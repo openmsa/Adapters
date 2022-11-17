05f22a8c1308cf50a3a3584c8e0eb60476c3714c
Antoine Brun <antoine.brun@gmail.com>
2022-11-17 17:48:03 +0100

    add debug

--- adapters/kubernetes_generic/kubernetes_generic_connect.php
+++ adapters/kubernetes_generic/kubernetes_generic_connect.php
@@ -189,6 +189,7 @@ class KubernetesGenericRESTConnection extends GenericConnection
             $raw_headers      = $headers_and_response[0];
             $response_body    = $headers_and_response[1];
             $response_headers = http_parse_headers($raw_headers);
+            debug_dump($response_headers, "response_headers:\n");
             if (array_key_exists('Content-Type', $response_headers)) {
                 $this->content_type = $response_headers['Content-Type'];
                 unset($headers_and_response[0]);
