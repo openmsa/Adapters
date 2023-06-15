2578f6eb485cf4976b48b780ffbca02efb4c1823
ArnaudFosse <34027417+ArnaudFosse@users.noreply.github.com>
2023-06-15 17:31:29 +0200

    Update kubernetes_generic_poll.php
    Add kapsule case for K8 polling

--- adapters/kubernetes_generic/polld/kubernetes_generic_poll.php
+++ adapters/kubernetes_generic/polld/kubernetes_generic_poll.php
@@ -33,6 +33,11 @@ switch ($sd->SD_CONFIGVAR_list['cloudService']->VAR_VALUE) {
       $end_point = $sd->SD_CONFIGVAR_list['KUBE_SERVER_IP']->VAR_VALUE;
     }
     break;
+  case 'kapsule':
+    if (!empty($sd->SD_CONFIGVAR_list['cluster_ip'])) {
+      $end_point = $sd->SD_CONFIGVAR_list['cluster_ip']->VAR_VALUE;
+    }
+    break;
   default:
     sms_log_error("Unknown cloudService\n");
     return ERR_CONFIG_VAR_UNDEFINED;
