ba114f5fcf27a52b869265b9ee957b6340a2a1c0
yguubiqube <ygu@ubiqube.com>
2023-08-21 12:34:27 +0200

    MSA-12810: fixes bad use of quotes in another script.

--- adapters/mon_generic/snmp_test.php
+++ adapters/mon_generic/snmp_test.php
@@ -16,7 +16,7 @@
 require_once 'smsd/sms_common.php';
 
 //$ret = exec_local(__FILE__ . ':' . __LINE__, "/opt/sms/bin/snmp_test.sh -t $snmp_cmd -H $ip_address -v 2c -c $snmp_community -o $oid", $output);
-$ret = exec_local(__FILE__ . ':' . __LINE__, "/opt/sms/bin/snmp_test.sh -t $snmp_cmd -H $ip_address -v 3 -c  -l authPriv -u TCNSS -a MD5 -A "4WAnk3c1L" -x DES -X "4WAnb3SaR" -o $oid", $output);
+$ret = exec_local(__FILE__ . ':' . __LINE__, "/opt/sms/bin/snmp_test.sh -t $snmp_cmd -H $ip_address -v 3 -c  -l authPriv -u TCNSS -a MD5 -A 4WAnk3c1L -x DES -X 4WAnb3SaR -o $oid", $output);
 if ($ret !== SMS_OK)
 {
   sms_send_user_error($sms_csp, $sdid, implode(' ' ,$output), ERR_SD_SNMP);
