657172e7713d836a2f9f3b84d0701fcc4e9f8605
dusky <dusyanthmaniks@gmail.com>
2023-11-25 10:57:40 +0530

    test

--- adapters/rest_generic/rest_generic_connect.php
+++ adapters/rest_generic/rest_generic_connect.php
@@ -169,7 +169,7 @@ class DeviceConnection extends GenericConnection {
 		curl_setopt($ch, CURLOPT_USERPWD, $auth_new);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $http_op);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array($headers_new));
-		curl_setopt($ch, CURLAUTH_AWS_SIGV4, $aws_sigv4_new);
+		curl_setopt($ch, CURLAUTH_SIV4, $aws_sigv4_new);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 50);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 50 );
 		if (count($cmd_list) >2 ) {
