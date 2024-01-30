b04429209fac8a9eee69a42a14673e34b1f04a35
Laurent Eyraud <43649021+eyraud@users.noreply.github.com>
2024-01-30 16:33:10 +0100

    add some space

--- adapters/genie_rest/rest_generic_apply_conf.php
+++ adapters/genie_rest/rest_generic_apply_conf.php
@@ -31,14 +31,14 @@ function rest_generic_apply_conf($configuration)
       {
         $res = sendexpectone(__FILE__ . ':' . __LINE__, $sms_sd_ctx, $line, '');
         $SMS_RETURN_BUF = json_encode($res);
-        $final_res= json_decode($SMS_RETURN_BUF , true); //Convert into json, the $res could be one SimpleXMLElement Object
+        $final_res = json_decode($SMS_RETURN_BUF , true); //Convert into json, the $res could be one SimpleXMLElement Object
         //debug_dump($final_res , "rest_generic_apply_conf.php DEVICE RESPONSE, SMS_RETURN_BUF=$SMS_RETURN_BUF;  final_res=\n");
         //{"response":{"status":"succ","result":{"data":{"row":{"id":"154","name":"test_16h56"}}}}}
         //{"response":{"status":"fail","err":{"msg":"Import Fail","code":"20007","description":{"error_data":{"row":{"name":"test_17h05_bad","message":{"row":" is not a valid IP address."}}}}}}}
-        if (isset($final_res["response"]) && isset($final_res["response"]["status"]) && $final_res["response"]["status"] =="fail" )
+        if (isset($final_res["response"]) && isset($final_res["response"]["status"]) && $final_res["response"]["status"] == "fail")
         {
            if (isset($final_res["response"]["err"])) {
-             $SMS_OUTPUT_BUF =  json_encode($final_res["response"]["err"]);
+             $SMS_OUTPUT_BUF = json_encode($final_res["response"]["err"]);
            } 
            return ERR_SD_CMDFAILED;
         }
