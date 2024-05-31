9f62d126f6a3ced6bb18f5d2c00c11dd58795614
Antoine Brun <antoine.brun@gmail.com>
2024-05-31 13:45:01 +0200

    fix Error: missing config vars for polling

--- adapters/vnfm_generic/polld/vnfm_generic_poll.php
+++ adapters/vnfm_generic/polld/vnfm_generic_poll.php
@@ -193,14 +193,31 @@ if (!empty($missing_conf_vars))
   sms_log_error("Error: missing config vars for polling: " . implode(', ', $missing_conf_vars));
   return ERR_SD_NOT_CONFIGURED;
 }
-$PROTOCOL        = $SD->SD_CONFIGVAR_list['PROTOCOL']->VAR_VALUE;
-$HTTP_PORT       = $SD->SD_CONFIGVAR_list['HTTP_PORT']->VAR_VALUE;
-$BASE_URL_MS     = trim($SD->SD_CONFIGVAR_list['BASE_URL_MS']->VAR_VALUE, '/');
-$SIGNIN_REQ_PATH = $SD->SD_CONFIGVAR_list['SIGNIN_REQ_PATH']->VAR_VALUE;
-$AUTH_TYPE = $SD->SD_CONFIGVAR_list['AUTH_MODE']->VAR_VALUE;
-$tenant_id = $SD->SD_CONFIGVAR_list['TENANT_ID']->VAR_VALUE;
-$user_domain_id = $SD->SD_CONFIGVAR_list['USER_DOMAIN_ID']->VAR_VALUE;
-$project_domain_id = $SD->SD_CONFIGVAR_list['PROJECT_DOMAIN_ID']->VAR_VALUE;
+
+if (isset($sd->SD_CONFIGVAR_list['PROTOCOL'])) {
+  $PROTOCOL        = $SD->SD_CONFIGVAR_list['PROTOCOL']->VAR_VALUE;
+}
+if (isset($sd->SD_CONFIGVAR_list['HTTP_PORT'])) {
+  $HTTP_PORT       = $SD->SD_CONFIGVAR_list['HTTP_PORT']->VAR_VALUE;
+}
+if (isset($sd->SD_CONFIGVAR_list['BASE_URL_MS'])) {
+  $BASE_URL_MS     = trim($SD->SD_CONFIGVAR_list['BASE_URL_MS']->VAR_VALUE, '/');
+}
+if (isset($sd->SD_CONFIGVAR_list['SIGNIN_REQ_PATH'])) {
+  $SIGNIN_REQ_PATH = $SD->SD_CONFIGVAR_list['SIGNIN_REQ_PATH']->VAR_VALUE;
+}
+if (isset($sd->SD_CONFIGVAR_list['AUTH_MODE'])) {
+  $AUTH_TYPE = $SD->SD_CONFIGVAR_list['AUTH_MODE']->VAR_VALUE;
+}
+if (isset($sd->SD_CONFIGVAR_list['TENANT_ID'])) {
+  $tenant_id = $SD->SD_CONFIGVAR_list['TENANT_ID']->VAR_VALUE;
+}
+if (isset($sd->SD_CONFIGVAR_list['USER_DOMAIN_ID'])) {
+  $user_domain_id = $SD->SD_CONFIGVAR_list['USER_DOMAIN_ID']->VAR_VALUE;
+}
+if (isset($sd->SD_CONFIGVAR_list['PROJECT_DOMAIN_ID'])) {
+  $project_domain_id = $SD->SD_CONFIGVAR_list['PROJECT_DOMAIN_ID']->VAR_VALUE;
+}
 
 // we get the bearer token
 $credentials = array('grant_type' => 'client_credentials',
@@ -214,6 +231,7 @@ if($AUTH_TYPE == 'keystone'){
 
 $token = get_token($SIGNIN_REQ_PATH, $credentials, $AUTH_TYPE);
 // sms_log_info(basename(__FILE__, '.php') . " polling token: $token");
+echo("token : ".$token."\n");
 
 $ret = get_health($PROTOCOL, $ME_IP, $HTTP_PORT, $BASE_URL_MS, $token, $AUTH_TYPE);
 
