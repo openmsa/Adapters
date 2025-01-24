fa21c938d9d22bdbda8a1005eaf57e3c2c6b03ab
Vincent MONNIER <80886295+vmonnier@users.noreply.github.com>
2025-01-24 17:21:00 +0100

    Update kubernetes_generic_poll.php again

--- adapters/kubernetes_generic/polld/kubernetes_generic_poll.php
+++ adapters/kubernetes_generic/polld/kubernetes_generic_poll.php
@@ -29,21 +29,16 @@ switch ($sd->SD_CONFIGVAR_list['cloudService']->VAR_VALUE) {
     }
     break;
   case 'kubernetes':
-    $end_point="";
-    if (!empty($sd->SD_CONFIGVAR_list['KUBE_HTTP_PROTOCOL'])) {
-      $end_point = $sd->SD_CONFIGVAR_list['KUBE_HTTP_PROTOCOL']."/";
-    }
-    if (!empty($sd->SD_CONFIGVAR_list['KUBE_SERVER_IP'])) {
-      $end_point = $end_point.$sd->SD_CONFIGVAR_list['KUBE_SERVER_IP']->VAR_VALUE;
-    }
-    if (!empty($sd->SD_CONFIGVAR_list['KUBE_PORT'])) {
-      $end_point = $end_point.":".$sd->SD_CONFIGVAR_list['KUBE_PORT'];
-    }
-    break;
-  case 'kapsule':
-    if (!empty($sd->SD_CONFIGVAR_list['cluster_ip'])) {
-      $end_point = $sd->SD_CONFIGVAR_list['cluster_ip']->VAR_VALUE;
-    }
+ 	  $end_point='';
+	  if (!empty($sd->SD_CONFIGVAR_list['KUBE_HTTP_PROTOCOL'])) {
+		  $end_point = $sd->SD_CONFIGVAR_list['KUBE_HTTP_PROTOCOL']->VAR_VALUE.'://';
+	  }
+	  if (!empty($sd->SD_CONFIGVAR_list['KUBE_SERVER_IP'])) {
+		  $end_point .= $sd->SD_CONFIGVAR_list['KUBE_SERVER_IP']->VAR_VALUE;
+	  }
+	  if (!empty($sd->SD_CONFIGVAR_list['KUBE_PORT'])) {
+		  $end_point .= ':'.$sd->SD_CONFIGVAR_list['KUBE_PORT']->VAR_VALUE;
+	  }
     break;
   case 'openstack':
     return SMS_OK;
